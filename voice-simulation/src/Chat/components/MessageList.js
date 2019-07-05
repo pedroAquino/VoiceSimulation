5c873d2c8fcca1fc3f4caef2a7afa46b2f5c2d30
Pedro Aquino <pedro.aquino@coxautoinc.com>
2019-07-05 15:20:39 -0300

    showing just first message

--- voice-simulation/src/Chat/components/MessageList.js
+++ voice-simulation/src/Chat/components/MessageList.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Message = ({ content }) => (
+const Message = ({ content, isVisible }) => isVisible ? (
     <div>
         <p>{content}</p>
     </div>
-);
+) : null;
 
 export default function MessageList({ messages }) {
     return (
         <div>
-            { messages.map(msg => <Message key={msg.id} content={msg.content} /> ) }
+            { messages.map(msg => <Message key={msg.id} content={msg.content} isVisible={msg.isVisible} /> ) }
         </div>
     );
 };
--- voice-simulation/src/Chat/services/messageFactory.js
+++ voice-simulation/src/Chat/services/messageFactory.js
@@ -15,16 +15,18 @@ export const buildInitialState = () => ({
             content: `Hi, I'm the dealertrack assistant, I'm here to help you with the simulation, can we start with the model of ther car you wanna buy ?`,
             id: 'vehicle_model',
             isVisible: true
-        },
-        {
-            content: `What's the year of the vehicle ?`,
-            id: 'vehicle_year',
-            isVisible: false
-        },
-        {
-            content: `What's the value of the entrance ?`,
-            id: 'vehicle_down_payment',
-            isVisible: false
         }
-    ].map(messageFactory).reverse()
+    ].map(messageFactory)
 })
+
+
+// {
+//     content: `What's the year of the vehicle ?`,
+//     id: 'vehicle_year',
+//     isVisible: false
+// },
+// {
+//     content: `What's the value of the entrance ?`,
+//     id: 'vehicle_down_payment',
+//     isVisible: false
+// }
