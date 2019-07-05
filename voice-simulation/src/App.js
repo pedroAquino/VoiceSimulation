edc03879c7f7201e317e4090a00052f8093d34de
Pedro Aquino <pedro.aquino@coxautoinc.com>
2019-07-05 11:39:51 -0300

    adding theme

--- voice-simulation/src/App.js
+++ voice-simulation/src/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import 'typeface-roboto';
+
+const theme = createMuiTheme({
+  palette: {
+    background: {
+        default: '#f5f5f5'
+    },
+    primary: {
+      light: '#fff',
+      main: '#0C4676',
+      dark: '#2a343d'
+    },
+    secondary: {
+        main: '#fff'
+    }
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
 
 function App() {
   return (
-    <div />
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <div />
+    </MuiThemeProvider>
   );
 }
 
