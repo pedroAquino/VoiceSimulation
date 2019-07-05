4a64a348ab8b5071671984a32df70351a2a78157
Pedro Aquino <pedro.aquino@coxautoinc.com>
2019-07-05 15:49:40 -0300

    creating bubble component to message

--- voice-simulation/src/Chat/components/MessageList.js
+++ voice-simulation/src/Chat/components/MessageList.js
@@ -1,15 +1,53 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Message = ({ content, isVisible }) => isVisible ? (
-    <div>
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+        minHeight: '100vh',
+        marginTop: -108,
+        padding: '0px 24px 0px 24px'
+    },
+    message: {
+        background: '#fff',
+        padding: 8,
+        borderRadius: 12,
+        position: 'relative',
+        '&:after': {
+            content: "''",
+            position: 'absolute',
+            borderStyle: 'solid',
+            borderWidth: '0 9px 11px',
+            borderColor: '#fff transparent',
+            display: 'block',
+            width: 0,
+            zIndex: 1,
+            top: -11,
+            left: 10
+        }
+    }
+}));
+
+const Message = ({ content, isVisible, className }) => isVisible ? (
+    <div className={className}>
         <p>{content}</p>
     </div>
 ) : null;
 
 export default function MessageList({ messages }) {
+    const classes = useStyles();
     return (
-        <div>
-            { messages.map(msg => <Message key={msg.id} content={msg.content} isVisible={msg.isVisible} /> ) }
+        <div className={classes.root} id="messages">
+            { messages.map(msg => (
+                <Message 
+                    className={classes.message}
+                    key={msg.id} 
+                    content={msg.content} 
+                    isVisible={msg.isVisible} 
+                />
+            ))}
         </div>
     );
 };
